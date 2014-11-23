08325de13dc3380f94d50528d1856b57cb3848d6
Sraddhanjali <sraddhanjali@picovico.com>
2014-11-23 10:17:24 +0545

    Add all

--- Xlib.py
+++
@@ -1,6 +0,0 @@
-from Xlib import X, display
-disp = display.Display()
-screen = disp.screen()
-root =  screen.root
-root.warp_pointer(100, 200)
-d.sync()
--- scale.py
+++
@@ -1,8 +0,0 @@
-width = capture.get(cv.CV_CAP_PROP_FRAME_WIDTH)
-height = capture.get(cv.CV_CAP_PROP_FRAME_HEIGHT)
-scalex = 1366/width
-scaley = 766/height
-window  = laser_center(original_frame)
-window = [scalex*window[0], scaley*window[1]]
-root.warp_pointer(window[0], window[1])
-d.sync()
